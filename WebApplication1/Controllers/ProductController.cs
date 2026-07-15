bca8491f3ae7eec706ff50eb37e73bd15a6c7767
MALEKARNIKITHA <malekarnikitha@gmail.com>
2026-07-15 13:43:52 +0530

    Initial Electronics API project

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using ElectronicsAPI.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+//using ElectronicsAPI.Models;
+//uisng Microsoft.AspNetCore.Mvc;
+
+namespace ElectronicsAPI.Models.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProductsController(AppDbContext context)
+        {
+            _context=context;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_context.Products.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+    }
+}
---
+++ WebApplication1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ElectronicsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace ElectronicsAPI.Models.Data
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+    }
+   }
+
